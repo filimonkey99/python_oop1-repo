ac2c3179a3292a2f48b3d644efa128f43e22c400
filimonkey99 <56698831+filimonkey99@users.noreply.github.com>
2022-01-18 14:53:15 +0100

    created inheretence file
    trying out classes with inheretence so that i get a better understandeing of coding in a object oriented way

--- student.py
+++ student.py
@@ -22,7 +22,11 @@ class Course:
         return False
 
     def get_average_grade(self):
-        pass
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+
+        return value / len(self.students)
 
 s1 = Student("Tim", 19, 95)
 s2 = Student("Bill", 19, 75)
@@ -31,5 +35,5 @@ s3 = Student("Jill", 19, 65)
 course = Course("Science", 2)
 course.add_student(s1)
 course.add_student(s2)
-
-print(course.students[0].name)
+print(course.add_student(s3))
+print(course.get_average_grade())
